4fd6b176aba0389a88a7513d81bd98b51ec2caf5
fullrestic <restinplastic@naver.com>
2025-08-20 11:23:15 +0900

    day5 : use dmypy

--- test.sh
+++ test.sh
@@ -4,22 +4,22 @@ COLOR_GREEN=`tput setaf 2;`
 COLOR_NC=`tput sgr0;` # No Color
 
 echo "Starting black"
-poetry run black .
+uv run black .
 echo "OK"
 
 echo "Starting ruff"
-poetry run ruff check --select I --fix
-poetry run ruff check --fix
+uv run ruff check --select I --fix
+uv run ruff check --fix
 echo "OK"
 
 echo "Starting mypy"
-poetry run mypy .
+uv run dmypy run -- .
 echo "OK"
 
 echo "Starting pytest with coverage"
-poetry run coverage run -m pytest
-poetry run coverage report -m
-poetry run coverage html
+uv run coverage run -m pytest
+uv run coverage report -m
+uv run coverage html
 
 echo "${COLOR_GREEN}All tests passed successfully!${COLOR_NC}"
 
